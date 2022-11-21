f90179c5feb8a17e851f393f42e00d5fd38c8ab3
원이설 <yinaw0202@gmail.com>
2022-11-21 16:48:23 +0900

    test

--- SmartMES_Giroei/P1B/P1B16_ITEM_BOX_RETURN_SUB.cs
+++ SmartMES_Giroei/P1B/P1B16_ITEM_BOX_RETURN_SUB.cs
@@ -133,7 +133,7 @@ namespace SmartMES_Giroei
                     lblMsg.Text = msg;
                     return;
                 }
-                //   INV_material_in 테이블에 다시 입력.
+                //   INV_material_in 테이블에 다시 입력. -> 이후에 변경해야 할 듯 하다.
                 sql = "insert into INV_material_out (mbarcode, barcode_surfix, prod_id, cust_id, input_date, plant, prodorder_id, output_date, qty, box_id, enter_man) " +
                     "values('" + mBarcode + "','" + sBarcode + "','" + sSubID + "','" + sCust + "','" + sDate + "','" + G.Pos + "'," + sSujuNo + "'," + DateTime.Now.ToString("yyyy-MM-dd") + "'," + sQty + ",'" + sBoxID + "','" + G.UserID + "')";
                     //+ " on duplicate key update" +
